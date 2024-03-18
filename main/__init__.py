884c48f1909ad5699ed821e472bd89900c9e36e9
haridas008 <156284133+haridas008@users.noreply.github.com>
2024-03-19 02:31:08 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,11 +12,11 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
+API_ID = "28031075"
+API_HASH = "6a309d42eda281416f798a554360e6a8"
+BOT_TOKEN = "6997680088:AAEXVoUhvAjeJsVkvay_j-jzj9z88TDF61w"
+SESSION = "AQGruGMAEihcxwnpBtjDh9jLPA2v1ruxUQ6zgI-32rfSQdlTD1UeExOzA00EhURoFw6BeIv7ddNMYnbZin-jhjyiE-O95Ey2TBxS8Ozhp6vZtUwVvx5bzyOlb36-LJBszXLkqxJZENM6e4c4rxxSNfXIBfJ_nkW9PDt6r_4i79Pszom0td7XhMjnf39uPwXHMrSfRs9dAZhHvPWsr0HUvTmnF1BFbfAlgdlIIS9uglFWr4jmFcXymbCxwbe37Osc-aa_yd7snPhiJ3DuHhHjp2YrLHr55laPUbNt2Z_UxBr9AWuVdVMfkGyF95Egewmh7XvCtqzgG68BA9kxeK1nHwsXWariegAAAAFbd0h7AA"
+FORCESUB = "vibrantsamundra"
 AUTH = config("AUTH", default=None, cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
